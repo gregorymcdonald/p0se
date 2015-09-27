f3d58eeac6ec78cff48ff2e02ae7a37d4f6a3fc3
aestrin <aestrin@utexas.edu>
2015-09-27 05:34:16 -0500

    Update: Greg is still fucking sleeping, I am a bit tired. Kieran is about to test that windows can execute a script. Pce brah. God Speed

--- src/P0seRecognizer/P0seRecognizer.java
+++ src/P0seRecognizer/P0seRecognizer.java
@@ -15,14 +15,14 @@ import colorutil.ColorTolerance;
 
 
 public class P0seRecognizer {
-	
+
 	Pose currentPose;
 	Boolean called = false;
-	
+
 	private static Color[] jointNodeColors = {
-        new Color(48, 114, 139),
-        new Color(167, 175, 71)
-    };
+		new Color(48, 114, 139),
+		new Color(167, 175, 71)
+	};
 
 	ArrayList <JointNode> jointNodes = new ArrayList<JointNode>();
 	/*
@@ -32,7 +32,7 @@ public class P0seRecognizer {
 	 */
 	public void updateNodeLocations(BufferedImage image){
 		jointNodes = new ArrayList<JointNode>();//resets the array for the new NodeJoints that will be captured in the new image
-		
+
 		for(int color = 0; color < jointNodeColors.length; color++){
 			Color currentColor = jointNodeColors[color];
 			JointNode node = new JointNode(ColorFinder.findColor(image, jointNodeColors[color], new ColorTolerance(60)), currentColor, color, new ColorTolerance(60));
@@ -40,10 +40,10 @@ public class P0seRecognizer {
 				jointNodes.add(node);
 			}	
 		}
-		
+
 		currentPose = new Pose(jointNodes);
 	}
-	
+
 	/*
 	 * Compares the current P0se against those of stored P0ses to see if there is a match
 	 * 
@@ -52,25 +52,35 @@ public class P0seRecognizer {
 	public void compareCurrentPose(){
 		//Pose currentPose = new Pose(jointNodes);
 		ArrayList<Double> testObject = new ArrayList<Double>();
-		
+
 		testObject.add(0.0);
 		testObject.add(270.0);
-		
+
 		Pose hardcodedPose = new Pose(testObject,1);
-		
+
 		System.out.println(currentPose.getAngles());
 		Runtime rt = Runtime.getRuntime();
-		
-		if(currentPose.equals(hardcodedPose, 30) && called == false){
-			 try {
-			        rt.exec(new String[]{"open", "-a", "Google Chrome", "--new", "--args"});
-			    	 //rt.exec(new String[]{"spotify_cmd Next"});
 
-			    } catch (IOException e) {
-			        // TODO Auto-generated catch block
-			        e.printStackTrace();
-			    }
-			 called = true;
+		if(currentPose.equals(hardcodedPose, 30) && called == false){
+			if(System.getProperty("os.name").startsWith("Windows")){
+				try {
+					rt.exec(new String[]{"cmd.exe","start", "chrome", "/new-window"});
+
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}else{
+				try {
+					rt.exec(new String[]{"open", "-a", "Google Chrome", "--new", "--args"});
+					//rt.exec(new String[]{"spotify_cmd Next"});
+
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+			called = true;
 		}else{
 			//System.out.println("Failed Test");
 		}
